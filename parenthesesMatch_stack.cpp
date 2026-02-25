2822929351398d6721bf5f96de9cfa9f1e310c13
Pranjul <pranjul.here@gmail.com>
2026-02-25 10:16:36 +0530

    added proper comments in the code

--- parenthesesMatch_stack.cpp
+++ parenthesesMatch_stack.cpp
@@ -6,31 +6,40 @@ using namespace std;
 
 bool isValidParentheses(string s) {
 
-  stack<char> st;
+  stack<char> st; // make an empty stack
 
+  // loop through string s
   for (char c : s) {
 
+    // if it is an opening bracket, push it on the stack
     if (c == '(' || c == '[' || c == '{') {
       st.push(c);
-    } else {
+    }
+
+    // if it is a closing bracket, check for a opening match
+    else {
       if (st.empty()) {
         return false;
       }
 
+      // look at the top most bracket of the stack
       char topBracket = st.top();
 
+      // check for a matching pair
       if ((c == ')' && topBracket == '(') || (c == ']' && topBracket == '[') ||
           (c == '}' && topBracket == '{')) {
-        st.pop();
+        st.pop(); // if it matches then, remove the opening bracket from stack
       }
 
       else {
-        return false;
+        return false; // there was a mismatch
       }
     }
   }
 
-  return st.empty();
+  return st
+      .empty(); // if our stack is empty at the end, then everything matched
+                // perfectly if it is not, then we have unclosed bracket left
 }
 
 int main() {
