3351f1dcb6bdd52ef8091e1ca1c8e18d49362e15
Pranjul <pranjul.here@gmail.com>
2026-03-02 13:39:17 +0530

    added a method to print pairs

--- dsa_java/freeCodeCamp.java
+++ dsa_java/freeCodeCamp.java
@@ -82,6 +82,15 @@ public class freeCodeCamp {
         }
     }
 
+    // Printing pairs O(n^2)
+    public static void printPairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[i] + " " + arr[j] + "\n");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 9, 8, 6, 3, 5, 1, 2, 7, 4 };
