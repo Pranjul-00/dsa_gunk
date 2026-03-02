0131a83bf2c3c94c50925a5c9268898812ba2933
Pranjul <pranjul.here@gmail.com>
2026-03-02 13:49:56 +0530

    fixed formatting

--- dsa_java/freeCodeCamp.java
+++ dsa_java/freeCodeCamp.java
@@ -86,7 +86,7 @@ public class freeCodeCamp {
     public static void printPairs(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                System.out.println(arr[i] + " " + arr[j] + "\n");
+                System.out.println(arr[i] + " " + arr[j]);
             }
         }
     }
