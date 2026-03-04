4673e4c2f88decb802286a47f8e16046db51a0b2
Pranjul <pranjul.here@gmail.com>
2026-03-04 23:52:54 +0530

    solving TwoSum

---
+++ dsa_java/TwoSums.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+public class TwoSums {
+
+    public static int[] twoSums(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(compliment), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 15, 11, 2, 7 };
+        int target = 9;
+
+        System.out.println("Target: " + target);
+        System.out.println("Array: " + Arrays.toString(nums));
+        System.out.println("---------------------------");
+
+        int[] optimalResult = twoSums(nums, target);
+        System.out.println("Optimal Result indices: ", Arrays.toString(optimalResult));
+
+        System.out.println("\nSorting using the freeCodeCamp.java file...");
+        freeCodeCamp.mergeSort(nums, 0, nums.length);
+        System.out.println("Sorted Array: ", Arrays.toString(nums));
+    }
+}
