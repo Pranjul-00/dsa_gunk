9d5a14b442e732a64afdc5e57b398f9d88d74e55
Pranjul <pranjul.here@gmail.com>
2026-03-05 00:40:50 +0530

    made a contains duplicate file

---
+++ dsa_java/containsDuplicate.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.swing.SpringLayout;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arrayWithDupes = { 1, 2, 3, 1 };
+        int[] arrayWithoutDupes = { 1, 2, 3, 4 };
+
+        System.out.println("Array 1: " + Arrays.toString(arrayWithDupes));
+        System.out.println("Contains Duplicate? : " + containsDuplicate(arrayWithDupes));
+
+        System.out.println("\nArray 2: " + Arrays.toString(arrayWithoutDupes));
+        System.out.println("Contains Duplicate? : " + containsDuplicate(arrayWithoutDupes));
+    }
+}
