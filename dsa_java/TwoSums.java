d8bbf6d4b57a519efc05979a29359c73176b4098
Pranjul <pranjul.here@gmail.com>
2026-03-04 23:58:57 +0530

    fixing some errors

--- dsa_java/TwoSums.java
+++ dsa_java/TwoSums.java
@@ -8,9 +8,9 @@ public class TwoSums {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+            int compliment = target - nums[i];
 
-            if (map.containsKey(complement)) {
+            if (map.containsKey(compliment)) {
                 return new int[] { map.get(compliment), i };
             }
             map.put(nums[i], i);
